1828be96d42c96af2c33c598af75d8f27d099dda
dongyeong <haedoji96@naver.com>
2021-06-15 19:18:08 +0900

    21/06/15

--- 1-3.knn_R.R
+++ 1-3.knn_R.R
@@ -14,7 +14,9 @@ euclidean_distance = function(a,b){
 }
 # euclidean_distance(1:10,11:20)
 
-nearest_neighbors = function(x, obs, k, FUN)
+
+## Find the k nearest neighbors
+nearest_neighbors = function(x, obs, k, FUN){
   
   # Check the number of observations is the same
   if (ncol(x) !=ncol(obs)){
@@ -24,4 +26,30 @@ nearest_neighbors = function(x, obs, k, FUN)
   dist = apply(x,1,FUN, obs)
   
   # Find closest
+  distances = sort(dist)[1:k]
+  neighbor_ind = which(dist %in% distances)
+  
+  if (length(neighbor_ind) != k){
+    warning(
+      paste('Several variables with equal distance. Used k:',length(neighbor_ind))
+    )
+  }
+  
+  ret = list(neighbor_ind,distances)
+  return(ret)
+}
 
+x = iris[1:nrow(iris)-1,]
+obs = iris[nrow(iris),]
+ind = nearest_neighbors(x[,1:4], obs[,1:4],3,euclidean_distance)[[1]]
+as.matrix(x[ind,1:4])
+# warning이 잘 작동함을 알 수 있다.
+
+## Prediction of the knn algorithm in classification problems
+knn_prediction = function(x,y){
+  
+  groups = table(x[,y])
+  pred = groups[groups==max(groups)]
+  return(pred)
+}
+knn_prediction(x[ind,], 'Species')
